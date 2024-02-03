3e4b3a9ac47779f50ba14cd10e830e8164c8c96c
Kaio Cavalcanti <kayollindoso@gmail.com>
2024-02-03 10:37:54 -0300

    Project created

---
+++ src/test/java/com/kaio/gerenciamento/GerenciamentoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kaio.gerenciamento;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GerenciamentoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
